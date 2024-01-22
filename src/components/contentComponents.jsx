6e8289cda0f6eef7e5b1b20f5460eab27d7e0b13
root <root@cv3510453.novalocal>
2024-01-23 01:37:46 +0300

    no front before connect | telegram description | toggle wallet

--- src/components/contentComponents.jsx
+++ src/components/contentComponents.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import {Card, FlexBoxRow, FlexBoxCol, Button, Ellipsis, Input} from "./styled/styled";
-import {getSiteData} from "../hooks/useBackend";
-import {Switch, TemplatePreview} from "./comps.jsx";
+import React, { useState } from 'react';
+import { Card, FlexBoxRow, FlexBoxCol, Button, Ellipsis, Input } from "./styled/styled";
+import { getSiteData } from "../hooks/useBackend";
+import { Switch, TemplatePreview } from "./comps.jsx";
 import './toggler.css';
-export const NoSiteContent = ({onSetProxy}) => (
+export const NoSiteContent = ({ onSetProxy }) => (
     <Card>
 
         <FlexBoxCol>
@@ -14,7 +14,7 @@ export const NoSiteContent = ({onSetProxy}) => (
 );
 
 
-export const SiteByTemplateContent = ({onSave, domain}) => {
+export const SiteByTemplateContent = ({ onSave, domain }) => {
     const [backgroundImage, setBackgroundImage] = useState('');
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
@@ -22,16 +22,22 @@ export const SiteByTemplateContent = ({onSave, domain}) => {
     const [telegramDetails, setTelegramDetails] = useState('');
     const [tonWallet, setTonWallet] = useState(true);
     const [displayNFTs, setDisplayNFTs] = useState(false);
+
     if (!title && !description) {
         getSiteData(domain).then((site_data) => {
             if (site_data) {
                 setTitle(site_data.title);
                 setDescription(site_data.description);
-                setTelegramDetails(site_data.telegramDetails);
+            }
+            console.log("SITE DATA\n\n\n", site_data)
+            if (site_data.contacts) {
+                console.log("Telegram Details")
+                console.log(site_data.contacts.wallet)
+                setTelegramDetails(site_data.contacts.telegram);
+                console.log(tonWallet)
             }
         })
     }
-
 
     return (
         <Card>
@@ -45,26 +51,26 @@ export const SiteByTemplateContent = ({onSave, domain}) => {
                         alignItems: "center",
                         justifyContent: "center",
                     }
-                }><TemplatePreview picture={"/tem1.gif"}/></div>
-                <div style={{display: 'grid', gap: '5px', margin: '5px 0px 5x 0px'}}>
+                }><TemplatePreview picture={"/tem1.gif"} /></div>
+                <div style={{ display: 'grid', gap: '5px', margin: '5px 0px 5x 0px' }}>
                     <label htmlFor="title">Title</label>
-                    <Input id="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                    <Input id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
                 </div>
-                <div style={{display: 'grid', gap: '5px', margin: '5px 0px 5px 0px'}}>
+                <div style={{ display: 'grid', gap: '5px', margin: '5px 0px 5px 0px' }}>
                     <label htmlFor="description">Description</label>
-                    <Input id="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
+                    <Input id="description" value={description} onChange={(e) => setDescription(e.target.value)} />
                 </div>
                 {/*<label htmlFor="picture">Picture (Optional)</label>*/}
                 {/*<Input id="picture" type="file" onChange={(e) => setPicture(e.target.files[0])} />*/}
-                <div style={{display: 'grid', gap: '5px', margin: '5px 0px 5px 0px'}}>
+                <div style={{ display: 'grid', gap: '5px', margin: '5px 0px 5px 0px' }}>
                     <label htmlFor="telegramDetails">Telegram Details</label>
                     <Input id="telegramDetails" value={telegramDetails}
-                        onChange={(e) => setTelegramDetails(e.target.value)}/>
+                        onChange={(e) => setTelegramDetails(e.target.value)} />
                 </div>
-                <div style={{display: 'grid', gap: '5px', margin: '5px 0px 10px 0px'}}>
+                <div style={{ display: 'grid', gap: '5px', margin: '5px 0px 10px 0px' }}>
                     <label htmlFor="tonWallet">TON Wallet</label>
                     {/*<Input id="tonWallet" value={tonWallet} onChange={(e) => setTonWallet(e.target.value)}/>*/}
-                    <Switch id="tonWallet" initialState={tonWallet} onToggle={(e) => setTonWallet(e)} />
+                    <Switch id="tonWallet" initialState={tonWallet} onToggle={(e) => setTonWallet(e.target)} />
                 </div>
                 {/*<Input id="displayNFTs" type="checkbox" checked={displayNFTs}*/}
                 {/*       onChange={(e) => setDisplayNFTs(e.target.checked)}/>*/}
@@ -85,7 +91,7 @@ export const SiteByTemplateContent = ({onSave, domain}) => {
 };
 
 
-export const ProxyContent = ({onSetProxy, domainRecord}) => {
+export const ProxyContent = ({ onSetProxy, domainRecord }) => {
     let [proxyUrl, setProxyUrl] = useState('');
     if (domainRecord && !proxyUrl) {
         proxyUrl = domainRecord;
@@ -97,21 +103,21 @@ export const ProxyContent = ({onSetProxy, domainRecord}) => {
                 <p>We allow you to set an ADNL address or a usual website here (in that case, we will proxy it for
                     you)</p>
                 <label htmlFor="proxyUrl">Enter URL to Proxy</label>
-                <Input id="proxyUrl" value={proxyUrl} onChange={(e) => setProxyUrl(e.target.value)}/>
+                <Input id="proxyUrl" value={proxyUrl} onChange={(e) => setProxyUrl(e.target.value)} />
                 <Button onClick={async () => await onSetProxy(proxyUrl)}>Set Proxy</Button>
             </FlexBoxCol>
         </Card>
     );
 };
 
-export const RedirectContent = ({onSetRedirect}) => {
+export const RedirectContent = ({ onSetRedirect }) => {
     const [redirectUrl, setRedirectUrl] = useState('');
 
     return (
         <Card>
             <FlexBoxCol>
                 <label htmlFor="redirectUrl">Enter URL to Redirect to</label>
-                <Input id="redirectUrl" value={redirectUrl} onChange={(e) => setRedirectUrl(e.target.value)}/>
+                <Input id="redirectUrl" value={redirectUrl} onChange={(e) => setRedirectUrl(e.target.value)} />
                 <Button onClick={async () => await onSetRedirect(redirectUrl)}>Set Redirect</Button>
             </FlexBoxCol>
         </Card>
