af6c3152ea0b8bab4bfa706251915a03a278d974
root <root@cv3510453.novalocal>
2024-02-09 13:49:34 +0300

    deleted broken logic

--- src/hooks/useTonConnect.ts
+++ src/hooks/useTonConnect.ts
@@ -6,7 +6,7 @@ import TonWeb from "tonweb";
 export function useTonConnect(): {
   connected: boolean;
   wallet: string | null;
-  sender: { send: (args: SenderArguments[]) => Promise<void> };
+  sender: { send: (args: SenderArguments) => Promise<void> };
   network: CHAIN | null
 } {
   const [tonConnectUI] = useTonConnectUI();
@@ -14,19 +14,26 @@ export function useTonConnect(): {
 
   return {
     sender: {
-      send: async (args: SenderArguments[]) => {
+      send: async (args: SenderArguments) => {
         // @ts-ignore
         var messages: SendTransactionRequest.messages = []
         var arg: SenderArguments
-        for (var i in args){
-          arg = args[i]
-          messages.push({
-            address: arg.to.toString(),
-            amount: arg.value.toString(),
-            // @ts-ignore
-            payload: TonWeb.utils.bytesToBase64(await arg.body?.toBoc(false))// args.body?.toBoc().toString("base64"),
-          })
-        }
+        // for (var i in args){
+        //   arg = args[i]
+        //   messages.push({
+        //     address: arg.to.toString(),
+        //     amount: arg.value.toString(),
+        //     // @ts-ignore
+        //     payload: TonWeb.utils.bytesToBase64(await arg.body?.toBoc(false))// args.body?.toBoc().toString("base64"),
+        //   })
+        // }
+        messages.push({
+          address: args.to.toString(),
+          amount: args.value.toString(),
+          // @ts-ignore
+          payload: TonWeb.utils.bytesToBase64(await args.body?.toBoc(false))// args.body?.toBoc().toString("base64"),
+        })
+
         tonConnectUI.sendTransaction({
           messages,
           validUntil: Date.now() + 5 * 60 * 1000, // 5 minutes for user to approve
